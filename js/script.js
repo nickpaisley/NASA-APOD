8fab316eaa12fa7f14d8d73ab8ee19d81b7a2b48
npaisley <2ep303@gmail.com>
2019-09-03 11:01:44 -0500

    creation

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var req = new XMLHttpRequest();
+var url = "https://api.nasa.gov/planetary/apod?api_key=";
+var api_key = "Fwh7Etm7HXFczX2kUYbl3AwQzirxygOrA2HZWgma";
+
+req.open("GET", url + api_key, true);
+req.send();
+
+req.addEventListener("load", function(){
+    if(req.status == 200 && req.readyState == 4){
+        var response = JSON.parse(req.responseText);
+    document.getElementById("title").textContent = response.title;
+    document.getElementById("date").textContent = response.date;
+    document.getElementById("pic").src = response.hdurl;
+    document.getElementById("explanation").textContent = response.explanation;
+    }
+
+})
