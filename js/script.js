5ab872840261618e96c60936e7ed010172aa103a
Nick Paisley <2ep303@gmail.com>
2019-09-08 15:31:47 -0500

    error handling complete

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var req = new XMLHttpRequest();
 var url = "https://api.nasa.gov/planetary/apod?api_key=";
-var api_key = "Fwh7Etm7HXFczX2kUYbl3AwQzirxygOrA2HZWgma";
+var api_key = "Fwh7Etm7HXFczX2kUYbl3AwQzirxygOrA2HZWgma&";
+var date = "date=2019-09-3";
 
 req.open("GET", url + api_key, true);
 req.send();
@@ -13,6 +14,19 @@ req.addEventListener("load", function () {
         document.getElementById("pic").src = response.hdurl;
         document.getElementById("vid").src = response.url;
         document.getElementById("explanation").textContent = response.explanation;
+
+        // This hides the image box if media_type is video
+        var img = document.getElementById("pic");
+        img.onerror = function () {
+            this.style.display = "none";
+        }
+
+        // Use JQUERY to check for CrossOrigin pic request, if media_type is Image, this hides the iframe
+        if (response.media_type == "image") {
+            $("#vid").css("display", "none");
+            $("#pic").attr("src", response.hdurl);
+        }
+
     }
 
 })
